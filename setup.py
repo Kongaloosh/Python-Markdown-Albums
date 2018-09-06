70cc0a25541e2e51a7f8ecc4d7f75ec289de6f33
kongaloosh <hi@kongaloosh.com>
2018-09-06 08:05:10 -0600

    renaming the package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="MarkdownAlbums",
     version=0.1,
-    packages=['markdown-albums'],
+    packages=['markdown_albums'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     install_requires=open('requirements.txt').read().split('\n'),
